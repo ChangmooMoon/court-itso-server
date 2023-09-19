2da0b0140b0d7f930fea670ddbfece026452ab42
Changmoo Moon <public.ellerymoon@gmail.com>
2023-09-19 22:37:04 +0900

    Initial commit

---
+++ src/main/java/com/court/courtitsoserver/CourtItsoServerApplication.java
@@ -0,0 +1,13 @@
+package com.court.courtitsoserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourtItsoServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourtItsoServerApplication.class, args);
+	}
+
+}
